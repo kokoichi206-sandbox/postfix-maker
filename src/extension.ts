e62c39a416e54f54a9db5b69523b452f5dd33190
takahiro.tominaga <tt.300607@gmail.com>
2023-10-13 22:27:52 +0900

    feat: normal postfix extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,52 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.languages.registerCompletionItemProvider(
+      {
+        language: "go",
+        scheme: "file",
+      },
+      {
+        provideCompletionItems(
+          document: vscode.TextDocument,
+          position: vscode.Position,
+          token: vscode.CancellationToken,
+          context: vscode.CompletionContext
+        ) {
+          let line = document.lineAt(position.line);
+          let dotIdx = line.text.lastIndexOf(".", position.character);
+          if (dotIdx === -1) {
+            return [];
+          }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "postfix-maker" is now active!');
+          let code = line.text.substring(
+            line.firstNonWhitespaceCharacterIndex,
+            dotIdx
+          );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('postfix-maker.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from postfix-maker!');
-	});
+          // len
+          let lengthSnippet = new vscode.CompletionItem("len");
+          lengthSnippet.additionalTextEdits = [
+          vscode.TextEdit.delete(
+            new vscode.Range(
+              position.translate(0, -(code.length + 1)),
+              position
+            )
+          ),
+          ];
+          lengthSnippet.insertText = new vscode.SnippetString(`len(${code})`);
+          lengthSnippet.documentation = 'my custom postfix completion';
+          lengthSnippet.kind = vscode.CompletionItemKind.Snippet;
+          lengthSnippet.sortText = "\u0000";
+          lengthSnippet.preselect = true;
 
-	context.subscriptions.push(disposable);
+          return [lengthSnippet];
+        },
+      },
+      "."
+    )
+  );
 }
 
 // This method is called when your extension is deactivated
